d4b7d5d4824b91c227865af2fb36ae691f4f1e23
MAHIRAJ SINGH BAGHELA <126379542+mahiraj7@users.noreply.github.com>
2024-04-29 16:41:14 +0530

    reordered tables

--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_24_074038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "notes", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.string "color"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_notes_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -34,6 +24,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_24_074038) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "color"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   add_foreign_key "notes", "users"
